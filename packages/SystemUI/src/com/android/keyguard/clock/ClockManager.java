dc425f71d49026c86dcecb2f2912d888ae4bddfd
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-07-29 12:19:48 +0200

    Clock Style : Add lockscreen Swing animation clock
    @ZeNiXxX
    * bring it up cause we can!

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -158,6 +158,11 @@ public final class ClockManager {
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BinaryClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new RainbowCircleClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new FireCircleClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
