abd467a9594d45666da2e5c1cec9d6acaccd0d0a
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-07-29 14:19:29 +0200

    Clock Style : Add lockscreen Rainbow circle animation clock
    @ZeNiXxX - Bringup

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -160,8 +160,8 @@ public final class ClockManager {
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new RainbowCircleClockController(res, layoutInflater, colorExtractor, context));
-        addBuiltinClock(() -> new FireCircleClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
+	addBuiltinClock(() -> new FireCircleClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
